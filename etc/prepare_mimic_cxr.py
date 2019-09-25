8fd1132ac56f3b17a54b75f00bfbf7df7ea5d6cf
Alistair Johnson <alistairewj@gmail.com>
2019-09-25 14:01:40 -0400

    fix up errors with empty sections

--- etc/prepare_mimic_cxr.py
+++ etc/prepare_mimic_cxr.py
@@ -3,10 +3,8 @@ import sys
 import os
 import argparse
 import csv
-import re
 from pathlib import Path
 
-import numpy as np
 from tqdm import tqdm
 
 # local folder import
@@ -17,10 +15,16 @@ parser = argparse.ArgumentParser()
 # Input report parameters.
 parser.add_argument('--reports_path',
                     required=True,
-                    help='Path to file with radiology reports.')
+                    help=('Path to file with radiology reports,'
+                          ' e.g. /data/mimic-cxr/files'))
 parser.add_argument('--output_path',
                     required=True,
                     help='Path to output CSV files.')
+
+
+def list_rindex(l, s):
+    """Helper function: *last* matching element in a list"""
+    return len(l) - l[-1::-1].index(s) - 1
 
 
 def main(args):
@@ -29,51 +33,98 @@ def main(args):
     reports_path = Path(args.reports_path)
     output_path = Path(args.output_path)
 
-    # get all the folders in MIMIC-CXR
-    p_folders = os.listdir(reports_path)
-    p_folders = [p for p in p_folders if p.startswith('p')]
-    p_folders.sort()
+    if not output_path.exists():
+        output_path.mkdir()
 
-    # For each patient
-    for p in tqdm(p_folders):
-        patient_path = reports_path / p
-        studies = os.listdir(patient_path)
-        studies = [s for s in studies
-                   if s.endswith('.txt') and s.startswith('s')]
+    # not all reports can be automatically sectioned
+    # we load in some dictionaries which have manually determined sections
+    custom_section_names, custom_indices = sp.custom_mimic_cxr_rules()
 
-        patient_studies = []
-        for s in studies:
-            with open(patient_path / s, 'r') as fp:
-                text = ''.join(fp.readlines())
+    # get all higher up folders (p00, p01, etc)
+    p_grp_folders = os.listdir(reports_path)
+    p_grp_folders = [p for p in p_grp_folders
+                     if p.startswith('p') and len(p) == 3]
+    p_grp_folders.sort()
 
-            # split text into sections
-            sections, section_names, section_idx = sp.split_report_into_sections(
-                text
-            )
+    patient_studies = []
+    for p_grp in p_grp_folders:
+        # the folders in MIMIC-CXR
+        cxr_path = reports_path / p_grp
+        p_folders = os.listdir(cxr_path)
+        p_folders = [p for p in p_folders if p.startswith('p')]
+        p_folders.sort()
 
-            # grab the *first* section with the given title
-            if 'impression' in section_names:
-                idx = section_names.index('impression')
-            elif 'findings' in section_names:
-                # If impression not found, extract findings
-                idx = section_names.index('findings')
-            elif 'last_paragraph' in section_names:
-                idx = section_names.index('last_paragraph')
-            else:
-                # If neither found, we do not output anything
-                patient_studies.append([s[0:-4], ''])
-                print(patient_path / s)
-                # sys.exit(0)
-                continue
+        # For each patient in this grouping folder
+        for p in tqdm(p_folders):
+            patient_path = cxr_path / p
+            studies = os.listdir(patient_path)
+            studies = [s for s in studies
+                       if s.endswith('.txt') and s.startswith('s')]
 
-            # Output into CSV files, one per patient
-            patient_studies.append([s[0:-4], sections[idx].strip()])
+            for s in studies:
+                with open(patient_path / s, 'r') as fp:
+                    text = ''.join(fp.readlines())
 
-        if len(patient_studies) > 0:
-            with open(output_path / f'{p}.csv', 'w') as fp:
+                # get study string name without the txt extension
+                s_stem = s[0:-4]
+
+                # custom rules for some poorly formatted reports
+                if s_stem in custom_indices:
+                    idx = custom_indices[s_stem]
+                    patient_studies.append([s_stem, text[idx[0]:idx[1]]])
+                    continue
+
+                # split text into sections
+                sections, section_names, section_idx = sp.section_text(
+                    text
+                )
+
+                # check to see if this has mis-named sections
+                # e.g. sometimes the impression is in the comparison section
+                if s_stem in custom_section_names:
+                    sn = custom_section_names[s_stem]
+                    idx = list_rindex(section_names, sn)
+                    patient_studies.append([s_stem, sections[idx].strip()])
+                    continue
+
+                # grab the *last* section with the given title
+                # prioritize impression > findings > last paragraph > comparison
+
+                # note comparison seems unusual but if no other sections
+                # exist the radiologist has usually written the
+                # report in the comparison section
+                idx = -1
+                for sn in ('impression', 'findings', 'last_paragraph', 'comparison'):
+                    if sn in section_names:
+                        idx = list_rindex(section_names, sn)
+                        break
+
+                if idx == -1:
+                    # we didn't find anything :(
+                    patient_studies.append([s_stem, ''])
+                    print(f'no impression/findings: {patient_path / s}')
+                else:
+                    # store the text of this section
+                    patient_studies.append([s_stem, sections[idx].strip()])
+
+            # if len(patient_studies) > 0:
+            #     with open(output_path / f'{p}.csv', 'w') as fp:
+            #         csvwriter = csv.writer(fp)
+            #         for row in patient_studies:
+            #             csvwriter.writerow(row)
+
+    # write distinct files to facilitate modular processing by chexpert
+    if len(patient_studies) > 0:
+        n = 0
+        jmp = 10000
+
+        while n < len(patient_studies):
+            n_fn = n // jmp
+            with open(output_path / f'mimic_cxr_{n_fn:03d}.csv', 'w') as fp:
                 csvwriter = csv.writer(fp)
-                for row in patient_studies:
+                for row in patient_studies[n:n+jmp]:
                     csvwriter.writerow(row)
+            n += jmp
 
 
 if __name__ == '__main__':
--- etc/section_parser.py
+++ etc/section_parser.py
@@ -2,7 +2,23 @@
 import re
 
 
-def split_report_into_sections(text):
+def section_text(text):
+    """Splits text into sections.
+
+    Assumes text is in a radiology report format, e.g.:
+
+        COMPARISON:  Chest radiograph dated XYZ.
+
+        IMPRESSION:  ABC...
+
+    Given text like this, it will output text from each section, 
+    where the section type is determined by the all caps header.
+
+    Returns a three element tuple:
+        sections - list containing the text of each section
+        section_names - a normalized version of the section name
+        section_idx - list of start indices of the text in the section
+    """
     p_section = re.compile(
         r'\n ([A-Z ()/,-]+):\s', re.DOTALL)
 
@@ -45,11 +61,27 @@ def split_report_into_sections(text):
 
     section_names = normalize_section_names(section_names)
 
+    # remove empty sections
+    # this handles when the report starts with a finding-like statement
+    #  .. but this statement is not a section, more like a report title
+    #  e.g. p10/p10103318/s57408307
+    #    CHEST, PA LATERAL:
+    #
+    #    INDICATION:   This is the actual section ....
+    # it also helps when there are multiple findings sections
+    # usually one is empty
+    for i in reversed(range(len(section_names))):
+        if section_names[i] in ('impression', 'findings'):
+            if sections[i].strip() == '':
+                sections.pop(i)
+                section_names.pop(i)
+                section_idx.pop(i)
+
     if ('impression' not in section_names) & ('findings' not in section_names):
         # create a new section for the final paragraph
         if '\n \n' in sections[-1]:
-            sections.append(sections[-1].split('\n \n')[-1])
-            sections[-2] = sections[-2].split('\n \n')[0:-1]
+            sections.append('\n \n'.join(sections[-1].split('\n \n')[1:]))
+            sections[-2] = sections[-2].split('\n \n')[0]
             section_names.append('last_paragraph')
             section_idx.append(section_idx[-1] + len(sections[-2]))
 
@@ -78,7 +110,7 @@ def normalize_section_names(section_names):
         "exam": "examination",  # 3907
         "clinical indication": "indication",  # 1945
         "conclusion": "impression",  # 1802
-        "chest, two views": "chest, two views",  # 1735
+        "chest, two views": "findings",  # 1735
         "recommendation(s)": "recommendations",  # 1700
         "type of examination": "examination",  # 1678
         "reference exam": "comparison",  # 347
@@ -138,7 +170,8 @@ def normalize_section_names(section_names):
         'bone window',
         'frontal upright',
         'frontal semi-upright',
-        'ribs'
+        'ribs',
+        'pa and lat'
     ]
     p_findings = re.compile('({})'.format('|'.join(p_findings)))
 
@@ -169,3 +202,69 @@ def normalize_section_names(section_names):
         # group similar phrasings for impression
 
     return section_names
+
+
+def custom_mimic_cxr_rules():
+    custom_section_names = {
+        's50913680': 'recommendations',  # files/p11/p11851243/s50913680.txt
+        's59363654': 'examination',  # files/p12/p12128253/s59363654.txt
+        's59279892': 'technique',  # files/p13/p13150370/s59279892.txt
+        's59768032': 'recommendations',  # files/p13/p13249077/s59768032.txt
+        's57936451': 'indication',  # files/p14/p14325424/s57936451.txt
+        's50058765': 'indication',  # files/p14/p14731346/s50058765.txt
+        's53356173': 'examination',  # files/p15/p15898350/s53356173.txt
+        's53202765': 'technique',  # files/p16/p16076182/s53202765.txt
+        's50808053': 'technique',  # files/p16/p16631485/s50808053.txt
+        's51966317': 'indication',  # files/p10/p10817099/s51966317.txt
+        's50743547': 'examination',  # files/p11/p11388341/s50743547.txt
+        's56451190': 'note',  # files/p11/p11842879/s56451190.txt
+        's59067458': 'recommendations',  # files/p11/p11984647/s59067458.txt
+        's59215320': 'examination',  # files/p12/p12408912/s59215320.txt
+        's55124749': 'indication',  # files/p12/p12428492/s55124749.txt
+        's54365831': 'indication',  # files/p13/p13876470/s54365831.txt
+        's59087630': 'recommendations',  # files/p14/p14267880/s59087630.txt
+        's58157373': 'recommendations',  # files/p15/p15032392/s58157373.txt
+        's56482935': 'recommendations',  # files/p15/p15388421/s56482935.txt
+        's58375018': 'recommendations',  # files/p15/p15505556/s58375018.txt
+        's54654948': 'indication',  # files/p17/p17090359/s54654948.txt
+        's55157853': 'examination',  # files/p18/p18975498/s55157853.txt
+        's51491012': 'history',  # files/p19/p19314266/s51491012.txt
+
+    }
+
+    custom_indices = {
+        's50525523': [201, 349],  # files/p10/p10602608/s50525523.txt
+        's57564132': [233, 554],  # files/p10/p10637168/s57564132.txt
+        's59982525': [313, 717],  # files/p11/p11989982/s59982525.txt
+        's53488209': [149, 475],  # files/p12/p12458657/s53488209.txt
+        's54875119': [234, 988],  # files/p13/p13687044/s54875119.txt
+        's50196495': [59, 399],  # files/p13/p13894879/s50196495.txt
+        's56579911': [59, 218],  # files/p15/p15394326/s56579911.txt
+        's52648681': [292, 631],  # files/p15/p15666238/s52648681.txt
+        's59889364': [172, 453],  # files/p15/p15835529/s59889364.txt
+        's53514462': [73, 377],  # files/p16/p16297706/s53514462.txt
+        's59505494': [59, 450],  # files/p16/p16730991/s59505494.txt
+        's53182247': [59, 412],  # files/p16/p16770442/s53182247.txt
+        's51410602': [47, 320],  # files/p17/p17069955/s51410602.txt
+        's56412866': [522, 822],  # files/p17/p17612000/s56412866.txt
+        's54986978': [59, 306],  # files/p17/p17912487/s54986978.txt
+        's59003148': [262, 505],  # files/p17/p17916384/s59003148.txt
+        's57150433': [61, 394],  # files/p18/p18335791/s57150433.txt
+        's56760320': [219, 457],  # files/p18/p18418794/s56760320.txt
+        's59562049': [158, 348],  # files/p18/p18502016/s59562049.txt
+        's52674888': [145, 296],  # files/p19/p19381919/s52674888.txt
+        's55258338': [192, 568],  # files/p13/p13719117/s55258338.txt
+        's59330497': [140, 655],  # files/p15/p15479218/s59330497.txt
+        's52119491': [179, 454],  # files/p17/p17959278/s52119491.txt
+        # below have no findings at all in the entire report
+        's58235663': [0, 0],  # files/p11/p11573679/s58235663.txt
+        's50798377': [0, 0],  # files/p12/p12632853/s50798377.txt
+        's54168089': [0, 0],  # files/p14/p14463099/s54168089.txt
+        's53071062': [0, 0],  # files/p15/p15774521/s53071062.txt
+        's56724958': [0, 0],  # files/p16/p16175671/s56724958.txt
+        's54231141': [0, 0],  # files/p16/p16312859/s54231141.txt
+        's53607029': [0, 0],  # files/p17/p17603668/s53607029.txt
+        's52035334': [0, 0],  # files/p19/p19349312/s52035334.txt
+    }
+
+    return custom_section_names, custom_indices
